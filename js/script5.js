67a18de634f63693887066c7405510656d59bbef
RukInDaHouse <r1997p@mail.ru>
2020-11-26 16:11:14 +0300

    script

--- js/script5.js
+++ js/script5.js
@@ -1,71 +1,31 @@
-console.clear();
+var map = document.getElementById("js-map");
+var districts = map.querySelectorAll("[class*=js-district]");
 
-var root  = document.documentElement;
-var body  = document.body;
-var animblocks = document.querySelectorAll(".animblock");
-var tiles = document.querySelectorAll(".tile");
-
-for (var i = 0; i < tiles.length; i++) {  
-  addListeners(tiles[i], animblocks[i]);
-}
-
-function addListeners(tile, animblock) {
-  
-  tile.addEventListener("click", function() {
-    animateHero(tile, animblock);
+var i = districts.length;
+while (i--) {
+  districts[i].addEventListener("mouseenter", function(e) {
+    if (e.target.classList.contains("js-district")) {
+      map.appendChild(e.target.parentNode);
+    }
   });
-  
-  animblock.addEventListener("click", function() {
-    animateHero(animblock, tile);
-  });  
-}
 
-function animateHero(fromHero, toHero) {
-    
-  var clone = fromHero.cloneNode(true);
-      
-  var from = calculatePosition(fromHero);
-  var to = calculatePosition(toHero);
-  
-  TweenLite.set([fromHero, toHero], { visibility: "visible", });
-  TweenLite.set(clone, { position: "absolute", margin: 0 });
-  
-  body.appendChild(clone);  
-      
-  var style = {
-    x: to.left - from.left,
-    y: to.top - from.top,
-    width: to.width,
-    height: to.height,
-    autoRound: false,
-    ease: Power1.easeOut,
-    onComplete: onComplete
-  };
-   
-  TweenLite.set(clone, from);  
-  TweenLite.to(clone, 3, style)
-    
-  function onComplete() {
-    
-    TweenLite.set(toHero, { visibility: "visible", });
-    body.removeChild(clone);
-  }
-}
+  districts[i].onclick = function(e) {
+    var svg = document.querySelector("svg");
+    var svgBounding = svg.getBBox();
 
-function calculatePosition(element) {
+    var mapBounding = map.getBBox();
+
+    var targetBounding = e.target.getBBox();
+
+    var scale = Math.min(mapBounding.width / targetBounding.width, mapBounding.height / targetBounding.height, 7);
+
+    var transformOriginX = targetBounding.x + targetBounding.width / 2;
+    var transformOriginY = targetBounding.y + targetBounding.height / 2;
+
     
-  var rect = element.getBoundingClientRect();
-  
-  var scrollTop  = window.animblockYOffset || root.scrollTop  || body.scrollTop  || 0;
-  var scrollLeft = window.animblockXOffset || root.scrollLeft || body.scrollLeft || 0;
-  
-  var clientTop  = root.clientTop  || body.clientTop  || 0;
-  var clientLeft = root.clientLeft || body.clientLeft || 0;
-    
-  return {
-    top: Math.round(rect.top + scrollTop - clientTop),
-    left: Math.round(rect.left + scrollLeft - clientLeft),
-    height: rect.height,
-    width: rect.width,
+
+    // ANIMATION
+    svg.style.transformOrigin = (transformOriginX) + "px " + (transformOriginY) + "px";
+    svg.style.transform = "scale(" + scale + ")";
   };
 }
